722fcf916f8eb3e03c8b29625d5e907d35c22ea1
ssk-tex <148786597+ssk-tex@users.noreply.github.com>
2023-10-24 10:20:23 +0530

    Add files via upload

---
+++ js-tut.js
@@ -0,0 +1,21 @@
+/* 
+    Code Editor:
+        1. Microsoft --> VS Code
+        2. JetBrains --> Fleet
+        3. Sublime Text
+*/
+
+/* 
+    JS environment --> 
+        1. node.js  [Developer: Ryan Dahl, OpenJS Foundation, Bryan Cantrill; Initial release date: 27 May 2009]
+        2. deno.js  [Developer: Ryan Dahl; Initial release date: 13 May 2018]
+
+*/
+
+/* 
+    To execute .js file in terminal --> node 'filename.js'
+*/
+
+
+/* ----------------------- GIT HUB --------------------------------- */
+
