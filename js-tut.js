35803a29d0175de4a387339a2f4d71d198b06eb7
samir-sk-nic <126137259+samir-sk-nic@users.noreply.github.com>
2023-10-24 19:37:08 +0530

    update 24-10-23

--- js-tut.js
+++ js-tut.js
@@ -16,7 +16,44 @@
     To execute .js file in terminal --> node 'filename.js'
 */
 
-
 /* ----------------------- GIT HUB --------------------------------- */
+// console.log('Hello Sir');  /* tesing on git desktop */
 
-console.log('Hello Sir');  /* tesing on git desktop */
+/* --------------------------- Variables ------------------------------- */
+
+/* 
+    Variables keywords --> 
+        1. const [ constant declaration, once a value set, can not change the value ]
+        2. let [ variable declaration ]
+        3. var [ Prefer not to use 'var' keyword, because od issue in block scope & functional scope ]
+*/
+
+// const Id = 123;
+// let email = 'abc@email.com'
+// var password = 'abc123'
+// city = 'kolkata' /* Not Allowed for declaration of variable */
+// Id = 3; /* Not Allowed For const keyword */
+
+// let state /* declare variable but not assign any value */
+
+// console.table([Id, email,password, city]); /* tabular structure console print */
+
+/* ------------------------- Datatypes -------------------------- */
+
+// "use strict"; /* treat all JS Code as newer version / strict mode */
+
+/* 
+    Datatypes --> 
+        1. String
+        2. Number
+        3. Boolean [ true / false ]
+        4. Array
+        5. NULL [ standalone value ]
+        6. undefined 
+        7. symbol [ unique ]
+
+        8. Object
+*/
+let name = "sunny"      /* string */
+let age = 30            /* number */
+let isLoggedIn = true   /* boolean */
