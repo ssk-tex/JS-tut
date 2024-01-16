2efced1e12e4006630808da74ce01e4c7e61bad7
samir-sk-nic <148786597+ssk-tex@users.noreply.github.com>
2024-01-16 21:36:31 +0530

    loop update

---
+++ loop.js
@@ -0,0 +1,46 @@
+/* 
+    for-of loop
+*/
+// const languages = ['c', 'c++', 'c#', 'java', 'js', 'php', 'r', 'python', 'ruby'];
+// for (const language of languages) {
+//     console.log(language);
+// }
+
+// const greetings = "Hello World";
+// for (const greet of greeting) {
+//     console.log(greet);
+// }
+
+/* 
+    Maps
+*/
+
+// const map = new Map();
+// map.set("IND", "India");
+// map.set("USA", "United States Of America");
+// map.set("NZ", "New Zealand");
+// map.set("AUS", "Australia");
+// map.set("UK", 'United Kingdom');
+// map.set("SA", 'South Africa');
+
+// // console.log(map);
+
+// for (const [key, val] of map) {
+//     console.log(`${key} : ${val} `);
+// }
+
+// const games = {
+//     game1 : 'nfs', game2 : 'igi'
+// }
+
+// for (const [game, name] of games) {
+//     console.log(`${game} : ${name}`);
+// }  /* Not iterable */
+
+const languageExtension = {
+    js : 'JavScript', cpp: 'C++', rb: 'Ruby', swift: 'Swift  by aplpe'
+}
+
+for (const key in object) {
+    
+}
